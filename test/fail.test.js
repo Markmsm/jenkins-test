6a80ee2aa7ecd624b89fd01bb8bb687ed6dc9bad
Mark <marksmunari@gmail.com>
2024-08-11 10:05:21 -0300

    adding failing test

--- test/fail.test.js
+++ test/fail.test.js
@@ -1,8 +1,8 @@
-// import { describe, it } from 'node:test'
-// import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
 
-// describe('Just make fail', () => {
-//     it('Fail test', () => {
-//         assert.deepStrictEqual(true, false)
-//     })
-// })
+describe('Just make fail', () => {
+    it('Fail test', () => {
+        assert.deepStrictEqual(true, false)
+    })
+})
